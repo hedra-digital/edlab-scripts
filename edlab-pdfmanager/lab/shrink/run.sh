86958b91a2fd69962e1f5c775d83b4fa804905b2
Jorge Sallum <jorge@hedra.com.br>
2024-11-30 14:18:27 -0300

    --convert-to-gray & --convert-to-black

---
+++ edlab-pdfmanager/lab/shrink/run.sh
@@ -0,0 +1,5 @@
+python shrink.py -i SEEDUCRJ-2024-001_CLEMENTINA_MIOLO_05ad28b.pdf --shrink 100
+python shrink.py -i SEEDUCRJ-2024-010_VASTO_MIOLO_e544c3e.pdf --shrink 100
+python shrink.py -i SEEDUCRJ-2024-011_ASAS_MIOLO_adda921.pdf --shrink 100
+python shrink.py -i SEEDUCRJ-2024-070_GANDHI_MIOLO.pdf --shrink 100
+python shrink.py -i SEEDUCRJ-2024-078_VOZES_MIOLO.pdf --shrink 100
