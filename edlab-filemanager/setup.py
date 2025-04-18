7a3dadb06ea5452acd5cce864022956e9d64bf40
Jorge Sallum <jorge@hedra.com.br>
2025-04-18 19:01:18 -0300

    filemanager

--- edlab-filemanager/setup.py
+++ edlab-filemanager/setup.py
@@ -7,17 +7,18 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "colorama",
+        "pathlib",
     ],
     entry_points={
         "console_scripts": [
-            "edlab-filemanager=filemanager.cli:main",
+            "edlab-filemanager=edlab_filemanager.cli:main",
         ],
     },
     python_requires=">=3.8",
     author="Your Name",
     author_email="your.email@example.com",
     description="File manager for renaming files based on a table or aligning numerical place values",
-    long_description=open("README.md").read(),
+    long_description="A tool for renaming files based on a table or aligning numerical place values",
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/edlab-filemanager",
     classifiers=[
